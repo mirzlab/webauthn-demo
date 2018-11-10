371d40b56c85648a080e9f3dffa5f76b49db8020
Vincent Mirzaian <vincent.mirzaian@gmail.com>
2018-11-10 17:59:27 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webauthnDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webauthnDemoApp
+ */
+angular.module('webauthnDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webauthnDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webauthnDemoApp
+ */
+angular.module('webauthnDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
